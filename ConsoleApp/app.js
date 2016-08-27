68a85d35ac6eec77ef34e54121173e98a05953d1
ZhangJin <nbnbnb.zhang@gmail.com>
2016-08-28 01:12:40 +0800

    Auto Compile

--- ConsoleApp/app.js
+++ ConsoleApp/app.js
@@ -2,34 +2,4 @@
 console.log();
 console.log("---------- start ---------- ");
 console.log("----------  end  ---------- ");
-class Animal {
-    constructor(theName) {
-        this.name = theName;
-    }
-    move(distanceInMeters = 0) {
-        console.log(`${this.name} moved ${distanceInMeters}m.`);
-    }
-}
-class Snake extends Animal {
-    constructor(name) {
-        super(name);
-    }
-    move(distanceInMeters = 5) {
-        console.log("Slithering...");
-        super.move(distanceInMeters);
-    }
-}
-class Horse extends Animal {
-    constructor(name) {
-        super(name);
-    }
-    move(distanceInMeters = 45) {
-        console.log("Galloping...");
-        super.move(distanceInMeters);
-    }
-}
-let sam = new Snake("Sammy the Python");
-let tom = new Horse("Tommy the Palomino");
-sam.move();
-tom.move(34);
 //# sourceMappingURL=app.js.map
--- ConsoleApp/app.ts
+++ ConsoleApp/app.ts
@@ -1,6 +1,5 @@
-"use strict"
+"use strict";
 console.log();
 console.log("---------- start ---------- ");
-
 
 console.log("----------  end  ---------- ");
