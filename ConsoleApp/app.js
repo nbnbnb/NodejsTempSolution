97c11889311ac50415ad92a329d4012b57dabd7c
ZhangJin <nbnbnb.zhang@gmail.com>
2017-03-12 19:09:52 +0800

    NN

--- ConsoleApp/app.js
+++ ConsoleApp/app.js
@@ -1,14 +1,3 @@
 console.log('----------start----------');
-var Point = (function () {
-    function Point() {
-    }
-    return Point;
-}());
-var JJJ = (function () {
-    function JJJ(zzz) {
-        this.zzz = zzz;
-    }
-    return JJJ;
-}());
 console.log('---------- end ----------');
 //# sourceMappingURL=app.js.map
